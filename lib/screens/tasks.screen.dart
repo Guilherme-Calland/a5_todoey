27ced9f330242a9af30b04cfaa2154a35b28c178
guilhermow <gui.crb0@gmail.com>
2020-05-15 14:20:48 -0300

    grinding out scoll view

--- lib/screens/tasks.screen.dart
+++ lib/screens/tasks.screen.dart
@@ -30,7 +30,7 @@ class TasksScreen extends StatelessWidget {
             Guilherme()
           ],
         ),
-      )
+      ),
     );
   }
 }
--- lib/widgets/todoey.listview.dart
+++ lib/widgets/todoey.listview.dart
@@ -6,24 +6,52 @@ class TodoeyListView extends StatelessWidget {
   List < String > tasks = [
     'do the dishes',
     'buy some cheese',
-    'conquer the world'
+    'conquer the world',
+    'do the dishes',
+    'buy some cheese',
+    'conquer the world',
+    'do the dishes',
+    'buy some cheese',
+    'conquer the world',
+    'do the dishes',
+    'buy some cheese',
+    'conquer the world',
+    'do the dishes',
+    'buy some cheese',
+    'conquer the world',
+    'do the dishes',
+    'buy some cheese',
+    'conquer the world',
+    'do the dishes',
+    'buy some cheese',
+    'conquer the world',
+    'do the dishes',
+    'buy some cheese',
+    'conquer the world',
+    'do the dishes',
+    'buy some cheese',
+    'conquer the world',
   ];
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ListView.builder(
-        itemCount: tasks.length,
-        itemBuilder: (context, index){
-          return ListTile(
-            trailing: Checkbox(
-              activeColor: todoeyTheme,
-              value: false,
-              onChanged: (bool newValue){}, 
-            ),
-            title: Text(tasks[index])
-          );
-        }
+      child: SingleChildScrollView(
+        child: ListView.builder(
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          itemCount: tasks.length,
+          itemBuilder: (context, index){
+            return ListTile(
+              trailing: Checkbox(
+                activeColor: todoeyTheme,
+                value: false,
+                onChanged: (bool newValue){}, 
+              ),
+              title: Text(tasks[index])
+            );
+          }
+        ),
       ),
     );
   }

e89a0397d2bfb5525c2bcae4e78bd5a3c924685c
guilhermow <gui.crb0@gmail.com>
2020-05-15 18:08:21 -0300

    add task screen

---
+++ lib/screens/add.task.screen.dart
@@ -0,0 +1,91 @@
+import 'package:a5_todoey/resources/resources.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animator/widgets/animator_widget.dart';
+import 'package:flutter_animator/widgets/sliding_entrances/slide_in_up.dart';
+
+class AddTaskScreen extends StatelessWidget {
+
+  final GlobalKey<AnimatorWidgetState> slideInUpKey =
+  GlobalKey<AnimatorWidgetState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideInUp(
+      key: slideInUpKey,
+        child: Transform.translate(
+        offset: Offset(0, -100),
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(20)
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                ),
+              ],
+            ),
+            padding: EdgeInsets.all(15),
+            height: 210,
+            width: 300,
+            child: Center(
+              child: Material(
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Add Task',
+                      style: TextStyle(
+                        color: todoeyTheme,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w900
+                      )
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      // autofocus: true,
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                      decoration: InputDecoration(   
+                        enabledBorder: UnderlineInputBorder(      
+                          borderSide: BorderSide(color: todoeyTheme),   
+                        ),  
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: todoeyTheme),
+                        ),
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: todoeyTheme),
+                        ),
+                      )
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    FlatButton(
+                      color: todoeyTheme,
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        'Add',
+                        style: TextStyle(
+                          color: Colors.white
+                        )
+                      ),
+                      onPressed: (){},
+                    )
+                  ],
+                )
+              )          
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -1,9 +1,12 @@
 import 'package:a5_todoey/resources/resources.dart';
+import 'package:a5_todoey/screens/add.task.screen.dart';
 import 'package:a5_todoey/screens/tasks.screen.dart';
 import 'package:a5_todoey/screens/title.screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,36 +35,6 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class AddTaskScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Material(
-        color: todoeyTheme,
-        borderRadius: BorderRadius.all(
-          Radius.circular(40)
-        ),
-        elevation: 5,
-        shadowColor: Colors.grey,
-        child: Padding(
-          padding: const EdgeInsets.all(40.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Add Task',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w400
-                ),)
-            ],
-          )
-        ),
-      ),
-    );
-  }
-}
 
 
 

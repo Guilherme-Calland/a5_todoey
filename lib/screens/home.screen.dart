f8fdf9911d1b7078949abacac31819def8a3ca53
guilhermow <gui.crb0@gmail.com>
2020-05-15 16:10:09 -0300

    grinding out the add tasks screen

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -15,7 +15,50 @@ class HomeScreen extends StatelessWidget {
           TitleScreen(),
           TasksScreen(),
         ],
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: todoeyTheme,
+        child: Icon(Icons.add),
+        onPressed: (){
+          showModalBottomSheet(
+            context: context, 
+            builder: (BuildContext context){
+              return SingleChildScrollView(
+                padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: Container(
+                  color: Color(0xff757575),
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: todoeyTheme),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)
+                      ),
+                      color: todoeyTheme,
+                      
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Add Task',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w900
+                          )
+                        )
+                      ],
+                    )
+                  ),
+                )
+              );
+            }
+          );
+        },
+      ),
     );
   }
 }
